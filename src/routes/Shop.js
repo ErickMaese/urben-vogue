4a2a69500463dbe50ac53213092428e3eb7c51b5
Erick Maese <maese99@gmail.com>
2023-04-12 20:00:35 -0700

    set up home page update

--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Newsletter from '../components/Newsletter';
 
 const Shop = () => {
 	return (
 		<div>
 			<Navbar />
+			<Newsletter />
 			<Footer />
 		</div>
 	);
